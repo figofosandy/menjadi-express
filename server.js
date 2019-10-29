870d0f8f21933a09755af7c3b1a4b8f6a275f03e
figofosandy <figofosandy@gmail.com>
2019-10-29 11:12:06 +0700

    menggunakan body-parser

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express=require('express') // importing module express
 const app=express(); 
 const port=3007
+const bodyParser=require('body-parser') // importing module body-parser
 
-// create route request get for root
-app.get('/',(req,res)=>res.send('Hello World!'))
+// konfigurasi body-parser
+app.use(bodyParser.urlencoded({extended:true})) // menangkap request dalam bentuk form url-encoded
+app.use(bodyParser.json()) // menangkap request dalam bentuk json
 
 // run server
 app.listen(port,()=>console.log(`Server listening on port ${port}`))
+
+// create route request get for root
+app.get('/',(req,res)=>res.send('Hello World!'))
 
 // create route request post for /hello
 app.post('/hello',function(req,res){
