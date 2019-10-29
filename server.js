22f28a298e5c05f52f5742bf5d5f89f19ab5ffa8
figofosandy <figofosandy@gmail.com>
2019-10-29 10:38:15 +0700

    membuat root method post

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const express=require('express')
-const app=express();
+const express=require('express') // importing module express
+const app=express(); 
 const port=3007
 
+// create route request get for root
 app.get('/',(req,res)=>res.send('Hello World!'))
 
+// run server
 app.listen(port,()=>console.log(`Server listening on port ${port}`))
+
+// create route request post for /hello
+app.post('/hello',function(req,res){
+    const respon={
+        statusCode:200,
+        error:"",
+        message:"Hello JSON"
+    }
+    res.json(respon)
+})
