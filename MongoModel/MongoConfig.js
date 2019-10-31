e1cd45f89d7f4481e0aab3e3d68d9794823f3c5b
figofosandy <55683150+figofosandy@users.noreply.github.com>
2019-10-31 15:18:33 +0700

    Update MongoConfig.js

--- MongoModel/MongoConfig.js
+++ MongoModel/MongoConfig.js
@@ -1,3 +1,3 @@
 const Mongoose=require('mongoose') // importing module mongoose
-Mongoose.connect('mongodb://localhost/belajarMongo') // konek ke server mongodb
+Mongoose.connect('mongodb+srv://admin:p4ssw0rd@cluster0-gkszh.mongodb.net/test?retryWrites=true&w=majority') // konek ke server mongodb
 module.exports=Mongoose
