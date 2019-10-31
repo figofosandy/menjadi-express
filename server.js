7e33b3b54cc90028994d8a6a28c2f640d6ff9fee
figofosandy <55683150+figofosandy@users.noreply.github.com>
2019-10-31 15:45:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -138,4 +138,4 @@ var todoRoute=require('./routes/todoRoute')
 app.use('/todo',todoRoute)
 
 // run server
-app.listen(port,()=>console.log(`Server listening on port ${port}`))
+app.listen(port,'0.0.0.0',()=>console.log(`Server listening on port ${port}`))
